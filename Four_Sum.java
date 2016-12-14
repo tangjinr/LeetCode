25ba132f8220bcb08cf1445171b531e9e1da9859
tangjinr <tangpersonal@163.com>
2016-12-14 21:28:02 +0800

    2016-12-14

--- Four_Sum.java
+++ Four_Sum.java
@@ -1,10 +1,41 @@
+/*
+ Author:     tangz, tangpersonal@163.com
+ Date:       Dec 13, 2014
+ Problem:    4Sum
+ Difficulty: Medium
+ Source:     https://leetcode.com/problems/4sum/
+ 
+ Describe:
+ 在一数组中，找出四个数之和等于target，输出这三个数a + b + c + d = target
+ 
+ Example:
+ Given S = [1, 0, -1, 0, -2, 2], and target = 0.
+ A solution set is:
+ [
+   [-1,  0, 0, 1],
+   [-2, -1, 1, 2],
+   [-2,  0, 0, 2]
+ ]
+ 
+ Type:数组
+ 
+ Solution:
+ 同3Sum，先排序，然后退化到两个数之和，利用夹逼寻找满足要求的nums[i]+nums[j]+nums[k]+nums[l]=target，需要去重
+ 这里介绍k个数之和等于target具体思想：
+ 方法一O(N^K)： 暴力，就是枚举所有的K-subset
+ 方法二O(n^(K-1))：
+ 基于排序，以及基于夹逼求两个数之和（O(NlogN)）的基础
+ 然后考虑，先确定一个数，再在剩下的数字中找到剩下的k-1个数，使他们之和等于target – 那个取出的数
+ k-1个数又可退化到求k-2个数，以此类推，最后只需要求得2个数之和
+ 
+ */
+
 package com.tang.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/*title:4Sum*/
 public class Four_Sum {
 
 	public static List<List<Integer>> fourSum(int[] nums, int target) {
